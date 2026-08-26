27c5943353b83394700c7952c0da0381ff3dfad2
kiranchitram <165831369+kiranchitram@users.noreply.github.com>
2026-08-26 11:31:17 +0530

    Update Help.jsx

--- DMBECP/frontend/src/pages/Help.jsx
+++ DMBECP/frontend/src/pages/Help.jsx
@@ -2,7 +2,7 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import "../styles/App.css";
 
-function Helppp() {
+function Help() {
   return (
     <div>
       <Navbar />
