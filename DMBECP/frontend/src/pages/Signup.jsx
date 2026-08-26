40ce77c6a1178c22ed24e00e298058c918c590db
kiranchitram <165831369+kiranchitram@users.noreply.github.com>
2026-08-26 11:32:42 +0530

    Update Signup.jsx

--- DMBECP/frontend/src/pages/Signup.jsx
+++ DMBECP/frontend/src/pages/Signup.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import "../styles/App.css";
 
-function Signupppp() {
+function Signup() {
   const [form, setForm] = useState({ name:"", username:"", email:"", password:"", phone:"" });
   const navigate = useNavigate();
 
