cae0f6f24e1999390cc0ea4620a208c46b7e72b8
kiranchitram <165831369+kiranchitram@users.noreply.github.com>
2026-08-26 11:28:56 +0530

    Update Home.jsx

--- DMBECP/frontend/src/pages/Home.jsx
+++ DMBECP/frontend/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import ProductCard from "../components/ProductCard";
 import Footer from "../components/Footer";
 import "../styles/App.css";
 
-function Home() {
+function Homeee() {
   // Electronics products with local images
   const products = [
     { name: "Laptop", image: "/images/electronics/laptop.webp" },
