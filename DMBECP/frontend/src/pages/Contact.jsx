8d7f0cc81402cd1f19f450699ac124d894e90509
kiranchitram <165831369+kiranchitram@users.noreply.github.com>
2026-08-26 11:28:01 +0530

    Update Contact.jsx

--- DMBECP/frontend/src/pages/Contact.jsx
+++ DMBECP/frontend/src/pages/Contact.jsx
@@ -2,7 +2,7 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import "../styles/App.css";
 
-function Contact() {
+function Contactttt() {
   return (
     <div>
       <Navbar />
